96e803b0fd92ef3bcecc35fc6bc54b2c6db84cf6
VitaliiBarabash1990 <alievruslan1818@gmail.com>
2025-12-27 17:36:32 +0100

    add bollit in services variant

--- src/components/Sections/Services/ServicesVariant/SwiperPhoto/SwiperPhoto.tsx
+++ src/components/Sections/Services/ServicesVariant/SwiperPhoto/SwiperPhoto.tsx
@@ -7,6 +7,7 @@ import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
+
 import s from "./SwiperPhoto.module.css";
 import SlideItem from "./SlideItem/SlideItem";
 
@@ -18,12 +19,18 @@ const SwiperPhoto: React.FC<HowItWorksProps> = ({ cardLists }) => {
 			<div className={s.sliderContainer}>
 				<Swiper
 					className={s.swiper}
+					modules={[Pagination, Navigation]}
+					loop
 					navigation={{
 						nextEl: ".services-next",
 						prevEl: ".services-prev",
 					}}
-					modules={[Pagination, Navigation]}
-					loop={true}
+					pagination={{
+						el: ".services-pagination",
+						clickable: true,
+						bulletClass: s.bullet,
+						bulletActiveClass: s.bulletActive,
+					}}
 					breakpoints={{
 						320: { slidesPerView: 1, spaceBetween: 8 },
 						768: { slidesPerView: 2, spaceBetween: 24 },
@@ -35,17 +42,23 @@ const SwiperPhoto: React.FC<HowItWorksProps> = ({ cardLists }) => {
 						</SwiperSlide>
 					))}
 				</Swiper>
+
+				{/* BULLETS + ARROWS (column-reverse вже враховано) */}
 				<div className={s.paginationBlock}>
-					<button className={`services-prev ${s.navButton} ${s.prevButton}`}>
-						<svg className={s.navButton_icon}>
-							<use href="/sprite.svg#icon-arrow-outlined"></use>
-						</svg>
-					</button>
-					<button className={`services-next ${s.navButton} ${s.nextButton}`}>
-						<svg className={`${s.navButton_icon} ${s.right}`}>
-							<use href="/sprite.svg#icon-arrow-outlined"></use>
-						</svg>
-					</button>
+					<div className={`services-pagination ${s.pagination}`}></div>
+
+					<div className={s.navWrapper}>
+						<button className={`services-prev ${s.navButton}`}>
+							<svg className={s.navButton_icon}>
+								<use href="/sprite.svg#icon-arrow-outlined" />
+							</svg>
+						</button>
+						<button className={`services-next ${s.navButton}`}>
+							<svg className={`${s.navButton_icon} ${s.right}`}>
+								<use href="/sprite.svg#icon-arrow-outlined" />
+							</svg>
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
